e7480963186ccc3bbda794079a61f1ce980bcc00
priyanka <priyanka150skct@gmail.com>
2019-07-25 11:31:20 +0530

    completed task6

--- src/main/java/com/stackroute/userservice/service/ApplicationStartupRunner.java
+++ src/main/java/com/stackroute/userservice/service/ApplicationStartupRunner.java
@@ -1,4 +1,15 @@
 package com.stackroute.userservice.service;
 
-public class ApplicationStartupRunner {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+
+public class ApplicationStartupRunner implements CommandLineRunner {
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("ApplicationStartupRunner run method Started !!");
+    }
 }
+
