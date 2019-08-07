ec72e7d85585725051448a857c26312783eb118e
Priyanka030997 <51943146+Priyanka030997@users.noreply.github.com>
2019-08-07 22:08:51 +0530

    return track object from save,update and delete method

--- src/main/java/com/stackroute/userservice/controller/TrackController.java
+++ src/main/java/com/stackroute/userservice/controller/TrackController.java
@@ -19,68 +19,48 @@ public class TrackController {
     {
         this.trackService = trackService;
     }
+    //handler method for save all the tracks
     @PostMapping(value="/save")
     public ResponseEntity<?> saveTrack(@RequestBody Track track)
     {
         ResponseEntity responseEntity;
-        try
-        {
-            trackService.saveTrack(track);
-            responseEntity=new ResponseEntity<String>("sucessfully created", HttpStatus.CREATED);
-        }
-        catch (Exception ex)
-        {
-            responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
+        try{
+//			calls saveTrack() from service
+            responseEntity = new ResponseEntity<Track>(trackService.saveTrack(track), HttpStatus.CREATED);
+        } catch (Exception e){
+            responseEntity = new ResponseEntity<String>("Exception", HttpStatus.CONFLICT);
         }
         return responseEntity;
     }
-
+//handler method for get all the tracks
     @GetMapping(value="/track")
     public ResponseEntity<?> getAllTracks()
     {
       return new ResponseEntity<List<Track>>(trackService.getAllTracks(),HttpStatus.OK);
     }
-
+//handler method for update the track
     @PostMapping(value="/update")
-    public ResponseEntity<?> updateTrack(@RequestBody Track track)
+    public ResponseEntity<?> updateTrack(@PathVariable int id,@RequestBody Track track)
     {
         ResponseEntity responseEntity;
-        try
-        {
-            trackService.updateTrack(track);
-            responseEntity=new ResponseEntity<String>("sucessfully updated", HttpStatus.CREATED);
-        }
-        catch (Exception ex)
-        {
-            responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
+        try{
+//			calls updateTrack() from service
+            responseEntity = new ResponseEntity<Track>(trackService.updateTrack(id,track), HttpStatus.OK);
+        } catch (Exception e){
+            responseEntity = new ResponseEntity<String>("Exception", HttpStatus.CONFLICT);
         }
         return responseEntity;
     }
+
+    //handler method for delete the track by given Id
     @DeleteMapping(value="/delete/{id}")
     public ResponseEntity<?> deleteTrackById(@PathVariable("id") int id)
     {
         ResponseEntity responseEntity;
         try
         {
-        trackService.deleteTrackById(id);
-        responseEntity=new ResponseEntity<String>("sucessfully deleted", HttpStatus.CREATED);
+        responseEntity=new ResponseEntity<Track>(trackService.deleteTrackById(id), HttpStatus.CREATED);
     }
-        catch(Exception ex)
-        {
-            responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
-        }
-        return responseEntity;
-    }
-
-    @GetMapping(value="/trackbyname/{name}")
-    public ResponseEntity<?> trackByName(@PathVariable("name") String name)
-    {
-        ResponseEntity responseEntity;
-        try
-        {
-            trackService.trackByName(name);
-            responseEntity=new ResponseEntity<String>("sucessfully trackByName", HttpStatus.CREATED);
-        }
         catch(Exception ex)
         {
             responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
@@ -88,3 +68,7 @@ public class TrackController {
         return responseEntity;
     }
 }
+
+    
+
+   
