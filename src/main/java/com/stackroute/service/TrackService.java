c0116e537c446a37fccb22848fbe12421e0c0464
Priyanka030997 <51943146+Priyanka030997@users.noreply.github.com>
2019-08-07 23:28:23 +0530

    Update TrackService.java

--- src/main/java/com/stackroute/service/TrackService.java
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -7,15 +7,15 @@ import com.stackroute.exceptions.TrackNotFoundException;
 import java.util.List;
 
 public interface TrackService {
-    public Track saveTrack(Track track) throws TrackAlreadyExistException;
+     Track saveTrack(Track track) throws TrackAlreadyExistException;
 
-    public List<Track> getAllTracks();
+    List<Track> getAllTracks();
 
     //public Track addNewTrack(Track track);
 
-    public Track updateTrack(Track track) throws TrackNotFoundException;
+     Track updateTrack(int id,Track track) throws TrackNotFoundException;
 
-    public void deleteTrackById(int id);
+     Track deleteTrackById(int id);
 
 }
 
