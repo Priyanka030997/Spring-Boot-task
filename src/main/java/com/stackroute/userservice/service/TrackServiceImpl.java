1e6a8a9f0a2da9a4d62037d1c7d957f530f66a78
Priyanka030997 <51943146+Priyanka030997@users.noreply.github.com>
2019-08-07 21:53:23 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/userservice/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/userservice/service/TrackServiceImpl.java
@@ -10,35 +10,48 @@ import java.util.List;
 @Service
 public class TrackServiceImpl implements TrackService {
 
-    TrackRepository trackRepository;
+       TrackRepository trackRepository;
     @Autowired
     public TrackServiceImpl(TrackRepository trackRepository)
     {
+
         this.trackRepository = trackRepository;
     }
+
+  //override the save method from TrackService class and this method save all the tracks
     @Override
-    public void saveTrack(Track track) {
-         trackRepository.save(track);
+    public Track saveTrack(Track track) {
+        return trackRepository.save(track);
 
     }
 
+    //override the getAllTracks method from TrackService class and this method get all the tracks
     @Override
     public List<Track> getAllTracks() {
+
         return trackRepository.findAll();
     }
-//    @Override
-//    public Track addNewTrack(Track track)
-//    {
-//        return trackRepository.save(track);
-//    }
+
+    //override the Update method from TrackService class and this method update the tracks
     @Override
-    public Track updateTrack(Track track)
-    {
-        return trackRepository.save(track);
+    public Track updateTrack(int id,Track track) {
+        if (trackRepository.existsById(id) == true) {
+            return trackRepository.save(track);
+        } else {
+            return null;
+        }
     }
+    //override the delete method from TrackService class and this method delete the track by given id
      @Override
-     public void deleteTrackById(int id)
+     public Track deleteTrackById(int id)
       {
-        trackRepository.deleteById(id);
+          Optional<Track> track =null;
+          if(trackRepository.existsById(id) == true)
+          {
+              trackRepository.deleteById(id);
+              track= trackRepository.findById(id);
+          }
+          return track.get();
        }
 }
+
